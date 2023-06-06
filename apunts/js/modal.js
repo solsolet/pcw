c38b3b832ca004d1961ae4728033742293238113
amsg18 <amsg18@alu.ua.es>
2023-06-06 11:22:24 +0200

    modal

---
+++ apunts/js/modal.js
@@ -0,0 +1,20 @@
+  
+function modal(){
+    let dialogo = document.createElement('dialog'),
+        html = '',
+        juega = "jugador";
+        html += `<h3>Modal</h3>
+            <p>Le toca a ${juega}</p>
+            <button onclick="cerrarDialogo(0);" class="boton">Cerrar</button>`;
+        
+
+    dialogo.innerHTML = html;
+    document.body.appendChild(dialogo);
+    dialogo.showModal();
+}
+
+function cerrarDialogo(valor){
+    document.querySelector('dialog').close(); //en açò NOMÉS no es borra del html
+    document.querySelector('dialog').remove(); //en açò si
+  }
+  
